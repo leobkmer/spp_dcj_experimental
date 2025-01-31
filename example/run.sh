df968dae7f907d38d50f153e7e2eb7093588c884
leobkmer <135108649+leobkmer@users.noreply.github.com>
2025-01-31 10:51:31 +0100

    Make example a bit nicer

--- example/run.sh
+++ example/run.sh
@@ -2,4 +2,4 @@
 python3 ../scripts/spp_dcj.py -a 1 -m idmap.txt tree.txt adjacencies.txt > example.ilp
 python3 ../scripts/compute_initial.py -a 1 tree.txt adjacencies.txt idmap.txt example_init.sol
 gurobi_cl InputFile=example_init.sol ResultFile=example.sol example.ilp 
-python3 ../scripts/sol2adjacencies.py example.sol idmap.txt 
+python3 ../scripts/sol2adjacencies.py example.sol idmap.txt  > resolved_adjacencies.txt
