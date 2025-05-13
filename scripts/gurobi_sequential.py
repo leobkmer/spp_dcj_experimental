f187ca649d1fe88f1d09edf745b96c9643b80df9
leobkmer <135108649+leobkmer@users.noreply.github.com>
2025-05-13 11:07:58 +0100

    Add solving strategy to workflow

--- scripts/gurobi_sequential.py
+++ scripts/gurobi_sequential.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3.7
 import gurobipy as gp
 import sys
 from argparse import ArgumentParser
@@ -8,6 +7,9 @@ parser = ArgumentParser()
 parser.add_argument("lpfile", help="File containing the (I)LP")
 parser.add_argument("solfile", help="File to write solution to.")
 parser.add_argument("-t",default=1,type=int, help="Number of threads to use.")
+parser.add_argument("--weightsolves",nargs="*",type=float,default=[0.1,0.5,0.9],help="Solve a weighted version of the ILP first.")
+parser.add_argument("--weightsolve-proportion",type=float,default=0.2,help="Maximum proportion of time spent on the weighted versions.")
+parser.add_argument("--timelim",type=int,default=3600,help="Overall time limit in seconds.")
 parser.add_argument("--tree",help="File containing the tree")
 parser.add_argument("--warm-start")
 #parser.add_argument("--timelim",type=int, default=60*60*24, help="Timelimit in seconds.")
@@ -19,15 +21,21 @@ f=model.getVarByName("f")
 if args.warm_start:
     model.read(args.warm_start)
     model.update()
-for alph in [0.1,0.9]:
-    model.Params.Timelimit = 500
+model.Params.MIPFocus=1
+model.Params.MIPGap=1/100
+for alph in args.weightsolves:
+    tl = args.weightsolve_proportion/len(args.weightsolves)*args.timelim
+    model.Params.Timelimit = tl
     ialph = alph -1
     model.setObjective(ialph*w+alph*f)
     model.update()
-    print("Optimizing with objective: ",model.getObjective())
+    print("-------------------WEIGHTED PRE-OPTIMIZATION:Optimizing with objective: ",model.getObjective()," and time limit {} ----------------------".format(tl))
     model.optimize()
+    print("Intermediate solution with f-objective: {}".format(f.X))
 model.setObjective(f)
-model.Params.Timelimit = 2600
+model.Params.Timelimit = args.timelim
+model.Params.MIPFocus=0
+model.Params.MIPGap=1e-4
 model.update()
 print("------------------FINAL OPTIMIZATION----------------")
 model.optimize()
