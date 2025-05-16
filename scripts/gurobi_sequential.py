68e4b19bab46b94862c2bf7ebc11bd921b4f8e17
babayagaofficial <64833238+babayagaofficial@users.noreply.github.com>
2025-05-16 19:28:27 +0100

    add memlimit to gurobi

--- scripts/gurobi_sequential.py
+++ scripts/gurobi_sequential.py
@@ -28,7 +28,6 @@ if args.tree:
 model = gp.read(args.lpfile)
 model.Params.Threads = args.t
 model.Params.SoftMemLimit = args.memlim/1000 - 5
-model.Params.Timelimit = args.timelim
 w=model.getVarByName("w")
 f=model.getVarByName("f")
 if args.warm_start:
@@ -46,6 +45,7 @@ for alph in args.weightsolves:
     model.optimize()
     #print("Intermediate solution with f-objective: {}".format(f.X))
 model.setObjective(f)
+model.Params.Timelimit = args.timelim
 model.Params.MIPFocus=0
 model.Params.MIPGap=1e-4
 model.update()
