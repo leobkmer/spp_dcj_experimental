9b58c7adb56d52eed3b03d307556e4d891d224e0
babayagaofficial <64833238+babayagaofficial@users.noreply.github.com>
2025-05-19 10:44:53 +0100

    Time limit needs to be set prior to any solving, as otherwise it hits the time limit on the cluster before hitting the time limit on solving, and errors out without saving any solutions

--- scripts/gurobi_sequential.py
+++ scripts/gurobi_sequential.py
@@ -18,6 +18,7 @@ args = parser.parse_args()
 model = gp.read(args.lpfile)
 model.Params.Threads = args.t
 model.Params.SoftMemLimit = args.memlim/1000 - 5
+model.Params.Timelimit = args.timelim
 w=model.getVarByName("w")
 f=model.getVarByName("f")
 if args.warm_start:
@@ -35,7 +36,6 @@ for alph in args.weightsolves:
     model.optimize()
     #print("Intermediate solution with f-objective: {}".format(f.X))
 model.setObjective(f)
-model.Params.Timelimit = args.timelim
 model.Params.MIPFocus=0
 model.Params.MIPGap=1e-4
 model.update()
