08acb06e8b1a4004a2b38075101d3957aa7638dc
Daria Frolova <64833238+babayagaofficial@users.noreply.github.com>
2026-04-22 14:23:26 +0200

    marker frequency and cost script

---
+++ scripts/cost_alg.py
@@ -0,0 +1,179 @@
+from ete3 import Tree
+#import pypangraph as pp
+import pandas as pd
+
+def read_block_intervals(pangraph_path, tree, min_len):
+    graph = pp.Pangraph.from_json(pangraph_path)
+    stats_df = graph.to_blockstats_df()
+    bl_count = graph.to_blockcount_df()
+    bl_count = bl_count.loc[bl_count.index.isin(stats_df[(stats_df["len"]>min_len) & (stats_df["core"]==False)].index)]
+    chr_to_plasmid = {chr.name:plasmid for chr in tree.iter_leaves() for plasmid in bl_count.columns if chr.name in plasmid}
+    all_trees = {block:tree.copy() for block in bl_count.index}
+    for block in all_trees.keys(): 
+        for leaf in all_trees[block].iter_leaves():
+            #states = {1 if bl_count.loc[block, chr_to_plasmid[leaf.name]]>0 else 0}
+            count = bl_count.loc[block, chr_to_plasmid[leaf.name]]
+            states = (count, count)
+            leaf.add_feature("states", states)
+    return list(stats_df[stats_df["core"]==True].index), all_trees
+
+def read_tree(filepath): #read in Newick tree with unlabelled internal nodes, and label the internal nodes
+    t = Tree(filepath, format=1)
+    i=1
+    for n in t.traverse():
+        if not n.is_leaf():
+            n.name = f"I_{i}"
+            i+=1
+    return t
+
+def recon_bottom_up(node, cost):
+    if node.is_leaf():
+        node.add_feature("possible_states", node.states)  # Assign leaf state
+    else:
+        left_states = recon_bottom_up(node.children[0], cost)
+        right_states = recon_bottom_up(node.children[1], cost)
+
+        # Ensure the possible_states attribute exists
+        node.add_feature("possible_states", dict())
+
+        if left_states[0]<=right_states[0]:
+            node.possible_states = (min(left_states[1], right_states[0]), max(left_states[1], right_states[0]))
+            cost[0] += max(0,right_states[0]-left_states[1])
+        else:
+            node.possible_states = (min(left_states[0], right_states[1]), max(left_states[0], right_states[1]))
+            cost[0] += max(0,left_states[0]-right_states[1])
+
+    return node.possible_states
+
+def recon_top_down(node, parent=None):
+
+    if node.is_leaf():
+        node.add_feature("final_states", node.possible_states)
+    elif parent is None:
+        node.add_feature("final_states", node.possible_states)
+    else:
+        node.add_feature("final_states", dict())
+        # Step I: Check if preliminary nodal set ⊆ parent final set, i.e. check if parent was formed through intersection
+        if parent.final_states[0]>=node.possible_states[0] and parent.final_states[1]<=node.possible_states[1]:
+            # Step II: Eliminate markers not in parent’s final set
+            node.final_states = parent.final_states
+        else:
+            # Step III: Determine whether it was a union or intersection
+            left_states = node.children[0].possible_states
+            right_states = node.children[1].possible_states
+            
+            # Step IV: add parental states shared with at least one child
+            if parent.final_states[1]<=node.possible_states[0]: #to the left
+                if left_states[0]<=right_states[0]: 
+                    final_set = (max(left_states[0], parent.final_states[0]), node.possible_states[1])
+                else:
+                    final_set = (max(right_states[0], parent.final_states[0]), node.possible_states[1])
+            else:
+                if left_states[0]<=right_states[0]:
+                    final_set = (node.possible_states[0], min(right_states[1], parent.final_states[1]))
+                else:
+                    final_set = (node.possible_states[0], min(left_states[1], parent.final_states[1]))
+
+            node.final_states = final_set
+
+    # Recursively process child nodes
+    for child in node.children:
+        recon_top_down(child, node)
+
+def reconstruction(tree):
+    """Executes both phases on an ete3 Tree."""
+    root = tree.get_tree_root()
+    score = [0]
+    recon_bottom_up(root, score)
+    recon_top_down(root)
+    return score
+
+def intervals_to_tsv(t, recon_trees, core, tsv):
+    with open(tsv, "w") as f:
+        f.write("genome\tfamily\tlower\thigher\n")
+        for block in recon_trees.keys():
+            tree = recon_trees[block]
+            for node in tree.traverse():
+                if not node.is_leaf():
+                    counts = (tree&node.name).final_states
+                    lower = counts[0]
+                    upper = counts[1]
+                    if not (lower==0 and upper==0):
+                        f.write(f"{node.name}\t{block}\t{lower}\t{upper}\n")
+        for node in t.traverse():
+            if not node.is_leaf():
+                for block in core:
+                    f.write(f"{node.name}\t{block}\t1\t1\n")
+
+def print_tree(node, level=0):
+    """Utility function to print the tree with assigned states."""
+    print("  " * level + f"{node.name}: {node.final_states}, {node.possible_states}")
+    for child in node.children:
+        print_tree(child, level + 1)
+
+def read_zombi(zombi_path, tree):
+    marker_set = {}
+    counts = {}
+    for leaf in tree.iter_leaves():
+        genome_df = pd.read_csv(f"{zombi_path}/Genomes/{leaf.name}_GENOME.tsv", sep="\t")
+        counts[leaf.name] = genome_df["GENE_FAMILY"].value_counts()
+        marker_set.add(set(counts.index))
+    multi_count = lambda gene: True if sum([(counts[leaf.name][gene]!=1) for leaf in tree.iter_leaves()]) != len(tree.iter_leave()) else False
+    accessory = [gene for gene in list(marker_set) if multi_count(gene)]
+    all_trees = {gene:tree.copy() for gene in accessory}
+    for gene in all_trees.keys(): 
+        for leaf in all_trees[gene].iter_leaves():
+            count = counts[leaf.name][gene]
+            states = (count, count)
+            leaf.add_feature("states", states)
+    return [gene for gene in list(marker_set) if not gene in accessory], all_trees
+
+def output_scores(scores, tsv):
+    with open(tsv, "w") as f:
+        f.write("block\tscore\n")
+        for block in scores.keys():
+            f.write(f"{block}\t{scores[block][0]}\n")
+
+newick = "(((A:1,B:1):1,(D:1,C:1):1):1,(E:1,F:1):1)Root;" 
+tree = Tree(newick, format=1)
+
+node = tree&"A"
+node.add_feature("states", (0,0))
+node = tree&"B"
+node.add_feature("states", (2,2))
+node = tree&"C"
+node.add_feature("states", (4,4))
+node = tree&"D"
+node.add_feature("states", (7,7))
+node = tree&"E"
+node.add_feature("states", (6,6))
+node = tree&"F"
+node.add_feature("states", (8,8))
+
+
+score = reconstruction(tree)
+
+print(score)
+print_tree(tree)
+
+scores = {}
+filepath = "blub" #path to tree
+tree = Tree(filepath, format=1)
+zombi = "zombi" #path to zombi output
+
+
+core, all_trees = read_zombi(zombi, tree)
+
+
+
+for block in all_trees.keys():
+    k = reconstruction(all_trees[block])
+    scores[block] = k
+
+intervals_to_tsv(tree, all_trees,core,"range.tsv")
+output_scores(scores, "scores.tsv")
+
+new_root = Tree()
+new_root.name = "Root"
+new_root.add_child(tree)
+tree.write(format=8, outfile="relabelled_trees.nw")
